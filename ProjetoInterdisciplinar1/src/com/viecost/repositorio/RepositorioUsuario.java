8f03a4f58e15603a16f30e331d804562e2a8ff7f
JoãoPaulo <joao_paulo_viegas11@hotmail.com>
2016-11-20 16:29:38 -0300

    repositorio

--- ProjetoInterdisciplinar1/src/com/viecost/repositorio/RepositorioUsuario.java
+++ ProjetoInterdisciplinar1/src/com/viecost/repositorio/RepositorioUsuario.java
@@ -1,14 +1,41 @@
 package com.viecost.repositorio;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import com.viecost.entidades.Usuario;
 
 public class RepositorioUsuario implements IRepositorioUsuario {
 
+	private Connection con = null;
+	private int id = 0;
+	
+	public void conecta(){
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		try{
+			 con = DriverManager.getConnection(url,"system","joao123123");
+			 
+		}catch(SQLException sql){
+			System.out.println("Erro na conexão" + sql);
+		}//fim do catch
+	}//fim do conecta
+	
+	public void desconecta(){
+		try{
+			con.close();
+		}catch(SQLException sql){
+			System.out.println("erro ao desconectar" +sql);
+		}//fim do catch
+	}//fim do desconecta
+	
+	
 	@Override
 	public void cadastrarUsuario(Usuario usuario) {
-		// TODO Auto-generated method stub
+		System.out.println("estou no repositorio BD");
+		
 		
 	}
 
